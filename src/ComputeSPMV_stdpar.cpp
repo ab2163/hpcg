ab850a3d92caee80b443abfc118748d389c013cc
Ajinkya Bhalerao <jinks.b99@gmail.com>
2025-06-12 17:48:48 +0100

    Made the stdpar code a bit more concise

--- src/ComputeSPMV_stdpar.cpp
+++ src/ComputeSPMV_stdpar.cpp
@@ -2,7 +2,6 @@
 #include <algorithm>
 #include <execution>
 #include <span>
-#include <numeric>
 
 #include "ComputeSPMV_stdpar.hpp"
 
@@ -16,28 +15,27 @@ int ComputeSPMV_stdpar(const SparseMatrix & A, Vector & x, Vector & y) {
   assert(x.localLength>=A.localNumberOfColumns); // Test vector lengths
   assert(y.localLength>=A.localNumberOfRows);
 
-#ifndef HPCG_NO_MPI
+  #ifndef HPCG_NO_MPI
     ExchangeHalo(A,x);
-#endif
+  #endif
+
   const double * const xv = x.values;
   double * const yv = y.values;
   const local_int_t nrow = A.localNumberOfRows;
 
-  // create a lazy-evaluated view
-  auto indices = std::ranges::views::iota(static_cast<local_int_t>(0), nrow);
-
-  // run parallel for with the lazy view
-  std::for_each(std::execution::par, indices.begin(), indices.end(), [&](local_int_t i) {
-    const double * const cur_vals = A.matrixValues[i];
-    const local_int_t * const cur_inds = A.mtxIndL[i];
-    const int cur_nnz = A.nonzerosInRow[i];
-
-    auto xv_view = std::views::transform(
-      std::span(cur_inds, cur_nnz),
-      [&xv](int idx) { return xv[idx]; }
+  // Create a view for xv values with "transform"
+  // Pass the view and the A values to "transform reduce"
+  std::for_each(std::execution::par, std::views::iota(0, nrow), [&](local_int_t i) {
+    yv[i] = std::transform_reduce(
+      A.matrixValues[i], 
+      A.matrixValues[i] + A.nonzerosInRow[i],
+      std::views::transform(
+        std::span(A.mtxIndL[i], A.nonzerosInRow[i]),
+        [xv](int idx) { return xv[idx]; }
+      ).begin(),
+      0.0
     );
-
-  yv[i] = std::transform_reduce(cur_vals, cur_vals+cur_nnz, xv_view.begin(), 0);
   });
+
   return 0;
 }
