5f49781c8a70c066cf606f779e5cc43b5ad780d0
Ajinkya Bhalerao <jinks.b99@gmail.com>
2025-06-16 14:38:16 +0100

    Changed top level shell script which allows compilation version to be specified now

--- ajinkya_build.sh
+++ ajinkya_build.sh
@@ -4,9 +4,25 @@ mkdir build
 cd build
 
 #Path on Azure instance:
-~/hpcg/configure ajinkya_laptop_linux
+#~/hpcg/configure ajinkya_laptop_linux
 
 #Path on laptop:
-#/home/ajinkya/Documents/hpcg/configure ajinkya_laptop_linux
-make
+/home/ajinkya/Documents/hpcg/configure ajinkya_laptop_linux
+
+#Run make and pass the relevant compilation flag
+case "$1" in
+  stdexec)
+    echo "Compiling for stdexec"
+    make USERFLAGS=-DSELECT_STDEXEC
+    ;;
+  stdpar)
+    echo "Compiling for stdpar"
+    make USERFLAGS=-DSELECT_STDPAR
+    ;;
+  *)
+    echo "Compiling for baseline"
+    make USERFLAGS=-DSELECT_BASELINE
+    ;;
+esac
+
 cd ..
