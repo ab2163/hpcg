d24fea8a7a1545cca4b3f3cb2b7cc3c49a1bec06
Ajinkya Bhalerao <jinks.b99@gmail.com>
2025-06-16 20:13:10 +0100

    Stdexec dot product drafted

--- src/ComputeDotProduct_stdexec.cpp
+++ src/ComputeDotProduct_stdexec.cpp
@@ -1,36 +1,44 @@
-//CURRENTLY SAME AS REFERENCE IMPLEMENTATION
-//CHANGE THIS CODE!
+#include <thread>
+#include <cassert>
+
+#include "../stdexec/include/stdexec/execution.hpp"
+#include "../stdexec/include/exec/static_thread_pool.hpp"
+#include "ComputeDotProduct_stdexec.hpp"
 
 #ifndef HPCG_NO_MPI
 #include <mpi.h>
 #include "mytimer.hpp"
 #endif
-#ifndef HPCG_NO_OPENMP
-#include <omp.h>
-#endif
-#include <cassert>
-#include "ComputeDotProduct_stdexec.hpp"
 
 int ComputeDotProduct_stdexec(const local_int_t n, const Vector & x, const Vector & y,
     double & result, double & time_allreduce) {
+
   assert(x.localLength>=n); // Test vector lengths
   assert(y.localLength>=n);
 
   double local_result = 0.0;
   double * xv = x.values;
   double * yv = y.values;
-  if (yv==xv) {
-#ifndef HPCG_NO_OPENMP
-    #pragma omp parallel for reduction (+:local_result)
-#endif
-    for (local_int_t i=0; i<n; i++) local_result += xv[i]*xv[i];
-  } else {
-#ifndef HPCG_NO_OPENMP
-    #pragma omp parallel for reduction (+:local_result)
-#endif
-    for (local_int_t i=0; i<n; i++) local_result += xv[i]*yv[i];
+
+  unsigned int num_threads = std::thread::hardware_concurrency();
+  if(num_threads == 0) {
+    std::cerr << "Unable to determine thread pool size.\n";
+    std::exit(EXIT_FAILURE);
   }
 
+  exec::static_thread_pool pool(num_threads);
+  auto sched = pool.get_scheduler();
+  auto start_point = stdexec::schedule(sched);
+
+  if (yv == xv) {
+    auto bulk_work = stdexec::bulk(start_point, n, [&](local_int_t i){ local_result += xv[i]*xv[i] });
+  }
+  else {
+    auto bulk_work = stdexec::bulk(start_point, n, [&](local_int_t i){ local_result += xv[i]*yv[i] });
+  }
+
+  stdexec::sync_wait(bulk_work);
+  
 #ifndef HPCG_NO_MPI
   // Use MPI's reduce function to collect all partial sums
   double t0 = mytimer();
