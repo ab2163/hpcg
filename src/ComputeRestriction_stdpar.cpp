06e1639fd8440b7f84ca288412d0879410ecd574
Ajinkya Bhalerao <jinks.b99@gmail.com>
2025-06-18 14:22:58 +0100

    Stdpar version of compute restriction added

--- src/ComputeRestriction_stdpar.cpp
+++ src/ComputeRestriction_stdpar.cpp
@@ -1,9 +1,6 @@
-//CURRENTLY SAME AS REFERENCE IMPLEMENTATION
-//CHANGE THIS CODE!
-
-#ifndef HPCG_NO_OPENMP
-#include <omp.h>
-#endif
+#include <ranges>
+#include <algorithm>
+#include <execution>
 
 #include "ComputeRestriction_stdpar.hpp"
 
@@ -15,10 +12,10 @@ int ComputeRestriction_stdpar(const SparseMatrix & A, const Vector & rf) {
   local_int_t * f2c = A.mgData->f2cOperator;
   local_int_t nc = A.mgData->rc->localLength;
 
-#ifndef HPCG_NO_OPENMP
-#pragma omp parallel for
-#endif
-  for (local_int_t i=0; i<nc; ++i) rcv[i] = rfv[f2c[i]] - Axfv[f2c[i]];
+  auto range = std::views::iota(0, nc);
 
+  std::for_each(std::execution::par, range.begin(), range.end(),
+              [&](int i) { rcv[i] = rfv[f2c[i]] - Axfv[f2c[i]]; });
+              
   return 0;
 }
