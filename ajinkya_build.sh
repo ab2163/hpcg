09a7859aa93fab30874c97fdec41d2733f138ae7
Ajinkya Bhalerao <jinks.b99@gmail.com>
2025-06-19 22:44:17 +0100

    Added host (laptop or azure) as build script argument

--- ajinkya_build.sh
+++ ajinkya_build.sh
@@ -3,11 +3,21 @@
 mkdir build
 cd build
 
-#Path on Azure instance:
-#~/hpcg/configure ajinkya_laptop_linux
-
-#Path on laptop:
-/home/ajinkya/Documents/hpcg/configure ajinkya_laptop_linux
+#Run make and pass the relevant compilation flag
+case "$2" in
+  azure)
+    echo "Using azure path"
+    ~/hpcg/configure ajinkya_laptop_linux
+    ;;
+  laptop)
+    echo "Using laptop path"
+    /home/ajinkya/Documents/hpcg/configure ajinkya_laptop_linux
+    ;;
+  *)
+    echo "Using laptop path"
+    /home/ajinkya/Documents/hpcg/configure ajinkya_laptop_linux
+    ;;
+esac
 
 #Run make and pass the relevant compilation flag
 case "$1" in
@@ -18,6 +28,10 @@ case "$1" in
   stdpar)
     echo "Compiling for stdpar"
     make USERFLAGS=-DSELECT_STDPAR
+    ;;
+  baseline)
+    echo "Compiling for baseline"
+    make USERFLAGS=-DSELECT_BASELINE
     ;;
   *)
     echo "Compiling for baseline"
