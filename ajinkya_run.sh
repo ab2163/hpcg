d3214c10062f6a9821780a628ab540bab4b2bf44
Ajinkya Bhalerao <jinks.b99@gmail.com>
2025-06-21 10:24:49 +0100

    Removed wait policy line from run shell script

--- ajinkya_run.sh
+++ ajinkya_run.sh
@@ -3,9 +3,6 @@
 cd build/bin
 export OMP_NUM_THREADS=4
 export OMP_PROC_BIND=true
-#Wait policy gives a 2X speedup on my laptop
-#and solves the 100/33/33/33 problem
-export OMP_WAIT_POLICY=active
 ./xhpcg
 cat HPCG-Benchmark_3.1*.txt
 cd ../..
