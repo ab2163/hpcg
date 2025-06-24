d787b8b813bd40ea42aff732bb2d79b353904025
Ajinkya Bhalerao <jinks.b99@gmail.com>
2025-06-24 07:46:44 +0100

    Trying to make the CG code work with modifications to... way in which each step in chain takes and receives senders

--- src/CG_stdexec.hpp
+++ src/CG_stdexec.hpp
@@ -17,7 +17,10 @@
 #define TICK()  t0 = mytimer() //!< record current time in 't0'
 #define TOCK(t) t += mytimer() - t0 //!< store time difference in 't' using time in 't0'
 
-template <stdexec::sender Sender>
+using stdexec::sender;
+using stdexec::then;
+
+template <sender Sender>
 decltype(auto) CG_stdexec(Sender input, const SparseMatrix & A, CGData & data, const Vector & b, Vector & x,
   const int max_iter, const double tolerance, int & niters, double & normr,  double & normr0,
   double * times, bool doPreconditioning){
@@ -41,16 +44,16 @@ decltype(auto) CG_stdexec(Sender input, const SparseMatrix & A, CGData & data, c
   if (print_freq < 1)  print_freq = 1;
 #endif
 
-  stdexec::sender auto current = input | stdexec::then([&](){
+  sender auto step1 = input | then([&](){
     //p is of length ncols, copy x to p for sparse MV operation
     CopyVector(x, p); 
   });
 
-  current = ComputeSPMV_stdexec(current, t3, A, p, Ap);
-  current = ComputeWAXPBY_stdexec(current, t2, nrow, 1.0, b, -1.0, Ap, r, A.isWaxpbyOptimized);
-  current = ComputeDotProduct_stdexec(current, t1, nrow, r, r, normr, t4, A.isDotProductOptimized);
+  sender auto step2 = ComputeSPMV_stdexec(step1, t3, A, p, Ap);
+  sender auto step3 = ComputeWAXPBY_stdexec(step2, t2, nrow, 1.0, b, -1.0, Ap, r, A.isWaxpbyOptimized);
+  sender auto step4 = ComputeDotProduct_stdexec(step3, t1, nrow, r, r, normr, t4, A.isDotProductOptimized);
 
-  current = stdexec::then(current, [&](){
+  sender auto step5 = then(step4, [&](){
     normr = sqrt(normr);
 #ifdef HPCG_DEBUG
     if (A.geom->rank == 0) HPCG_fout << "Initial Residual = "<< normr << std::endl;
@@ -62,32 +65,32 @@ decltype(auto) CG_stdexec(Sender input, const SparseMatrix & A, CGData & data, c
   //Start iterations
   //Convergence check accepts an error of no more than 6 significant digits of tolerance
   for(int k = 1; k <= max_iter && normr/normr0 > tolerance * (1.0 + 1.0e-6); k++){
-    current = stdexec::then(current, [&](){ TICK(); });
+    sender auto step6 = then((k > 1) ? step 20 : step5, [&](){ TICK(); });
     if(doPreconditioning)
-      current = ComputeMG_stdexec(current, dummy, A, r, z); //Apply preconditioner
+    sender auto step7 = ComputeMG_stdexec(step6, dummy, A, r, z); //Apply preconditioner
     else
-      current = stdexec::then(current, [&](){ CopyVector(r, z); }); //copy r to z (no preconditioning)
-    current = stdexec::then(current, [&](){ TOCK(t5); }); //Preconditioner apply time
+      sender auto step7 = then(step6, [&](){ CopyVector(r, z); }); //copy r to z (no preconditioning)
+    sender auto step8 = then(step7, [&](){ TOCK(t5); }); //Preconditioner apply time
 
     if(k == 1){
-      current = ComputeWAXPBY_stdexec(current, t2, nrow, 1.0, z, 0.0, z, p, A.isWaxpbyOptimized); //Copy Mr to p
-      current = ComputeDotProduct_stdexec(current, t1, nrow, r, z, rtz, t4, A.isDotProductOptimized); //rtz = r'*z
+      sender auto step9 = ComputeWAXPBY_stdexec(step8, t2, nrow, 1.0, z, 0.0, z, p, A.isWaxpbyOptimized); //Copy Mr to p
+      sender auto step12 = ComputeDotProduct_stdexec(step9, t1, nrow, r, z, rtz, t4, A.isDotProductOptimized); //rtz = r'*z
     }else{
-      current = stdexec::then(current, [&](){ oldrtz = rtz; });
-      current = ComputeDotProduct_stdexec(current, t1, nrow, r, z, rtz, t4, A.isDotProductOptimized); //rtz = r'*z
-      current = stdexec::then(current, [&](){ beta = rtz/oldrtz; });
-      current = ComputeWAXPBY_stdexec(current, t2, nrow, 1.0, z, beta, p, p, A.isWaxpbyOptimized); //p = beta*p + z
+      sender auto step9 = then(step8, [&](){ oldrtz = rtz; });
+      sender auto step10 = ComputeDotProduct_stdexec(step9, t1, nrow, r, z, rtz, t4, A.isDotProductOptimized); //rtz = r'*z
+      sender auto step11 = then(step10, [&](){ beta = rtz/oldrtz; });
+      sender auto step12 = ComputeWAXPBY_stdexec(step11, t2, nrow, 1.0, z, beta, p, p, A.isWaxpbyOptimized); //p = beta*p + z
     }
 
-    current = ComputeSPMV_stdexec(current, t3, A, p, Ap); //Ap = A*p
-    current = ComputeDotProduct_stdexec(current, t1, nrow, p, Ap, pAp, t4, A.isDotProductOptimized); //alpha = p'*Ap
-    current = stdexec::then(current, [&](){ alpha = rtz/pAp; });
-    current = ComputeWAXPBY_stdexec(current, t2, nrow, 1.0, x, alpha, p, x, A.isWaxpbyOptimized); //x = x + alpha*p
-    current = ComputeWAXPBY_stdexec(current, t2, nrow, 1.0, r, -alpha, Ap, r, A.isWaxpbyOptimized); //r = r - alpha*Ap
-    current = ComputeDotProduct_stdexec(current, t1, nrow, r, r, normr, t4, A.isDotProductOptimized);
-    current = stdexec::then(current, [&](){ normr = sqrt(normr); });
+    sender auto step13 = ComputeSPMV_stdexec(step12, t3, A, p, Ap); //Ap = A*p
+    sender auto step14 = ComputeDotProduct_stdexec(step13, t1, nrow, p, Ap, pAp, t4, A.isDotProductOptimized); //alpha = p'*Ap
+    sender auto step15 = then(step14, [&](){ alpha = rtz/pAp; });
+    sender auto step16 = ComputeWAXPBY_stdexec(step15, t2, nrow, 1.0, x, alpha, p, x, A.isWaxpbyOptimized); //x = x + alpha*p
+    sender auto step17 = ComputeWAXPBY_stdexec(step16, t2, nrow, 1.0, r, -alpha, Ap, r, A.isWaxpbyOptimized); //r = r - alpha*Ap
+    sender auto step18 = ComputeDotProduct_stdexec(step17, t1, nrow, r, r, normr, t4, A.isDotProductOptimized);
+    sender auto step19 = then(step18, [&](){ normr = sqrt(normr); });
     
-    current = stdexec::then(current, [&](){
+    sender auto step20 = then(step19, [&](){
 #ifdef HPCG_DEBUG
       if (A.geom->rank == 0 && (k % print_freq == 0 || k == max_iter))
         HPCG_fout << "Iteration = "<< k << "   Scaled Residual = "<< normr/normr0 << std::endl;
@@ -97,10 +100,10 @@ decltype(auto) CG_stdexec(Sender input, const SparseMatrix & A, CGData & data, c
 
     //because I am passing an lvalue reference to sync_wait
     //I will be able to resuse current afterwards
-    stdexec::sync_wait(current);
+    stdexec::sync_wait(step20);
   }
 
-  current = stdexec::then(current, [&](){
+  sender auto step21 = then(step20, [&](){
     //Store times
     times[1] += t1; //dot-product time
     times[2] += t2; //WAXPBY time
@@ -111,6 +114,6 @@ decltype(auto) CG_stdexec(Sender input, const SparseMatrix & A, CGData & data, c
     times[0] += mytimer() - t_begin;  //Total time. All done...
   });
 
-  return current;
+  return step20;
 }
 
