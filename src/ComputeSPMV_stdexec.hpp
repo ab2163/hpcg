7631f1278d175212404dadb0b736960ff9a978da
Ajinkya Bhalerao <jinks.b99@gmail.com>
2025-06-25 09:07:50 +0100

    Revised SPMV with new style

--- src/ComputeSPMV_stdexec.hpp
+++ src/ComputeSPMV_stdexec.hpp
@@ -1,8 +1,38 @@
+#include <cassert>
+#include <cstdlib>
+
 #include "../stdexec/include/stdexec/execution.hpp"
 #include "../stdexec/include/stdexec/__detail/__senders_core.hpp"
 #include "Vector.hpp"
 #include "SparseMatrix.hpp"
+#include "mytimer.hpp"
 
-template <stdexec::sender Sender>
-auto ComputeSPMV_stdexec(Sender input, double & time, const SparseMatrix & A, Vector  & x, Vector & y)
-  -> decltype(stdexec::then(input, [](){}));
+#ifndef HPCG_NO_MPI
+#include "ExchangeHalo.hpp"
+#endif
+
+auto ComputeSPMV_stdexec(double & time, const SparseMatrix & A, Vector  & x, Vector & y){
+  
+  return stdexec:then([&](){
+    if(time != NULL) time = mytimer();
+    assert(x.localLength >= A.localNumberOfColumns); //Test vector lengths
+    assert(y.localLength >= A.localNumberOfRows);
+#ifndef HPCG_NO_MPI
+    ExchangeHalo(A,x);
+#endif
+  })
+  | stdexec::bulk(stdexec::par, A.localNumberOfRows, [&](local_int_t i){
+    const double * const xv = x.values;
+    double * const yv = y.values;
+    yv[i] = std::transform_reduce(
+      A.matrixValues[i],
+      A.matrixValues[i] + A.nonzerosInRow[i],
+      std::views::transform(
+        std::span(A.mtxIndL[i], A.nonzerosInRow[i]),
+        [&xv](int idx) { return xv[idx]; }
+      ).begin(),
+      0.0
+    );
+  };)
+  | stdexec::then([&](){ if(time != NULL) time = mytimer() - time; });
+}
