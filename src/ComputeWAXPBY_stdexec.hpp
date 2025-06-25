1d339b99719ec9e22eaf51052d298088714bd578
Ajinkya Bhalerao <jinks.b99@gmail.com>
2025-06-25 09:55:25 +0100

    WAXPBY and SYMGS also revised

--- src/ComputeWAXPBY_stdexec.hpp
+++ src/ComputeWAXPBY_stdexec.hpp
@@ -1,7 +1,37 @@
+#include <thread>
+#include <cassert>
+#include <iostream>
+
 #include "../stdexec/include/stdexec/execution.hpp"
 #include "../stdexec/include/stdexec/__detail/__senders_core.hpp"
 #include "Vector.hpp"
+#include "mytimer.hpp"
 
-template <stdexec::sender Sender>
-auto ComputeWAXPBY_stdexec(Sender input, double & time, const local_int_t n, const double alpha, const Vector & x,
-    const double beta, const Vector & y, Vector & w) -> decltype(stdexec::then(input, [](){}));
+auto ComputeWAXPBY_stdexec(double & time, const local_int_t n, const double alpha, const Vector & x,
+    const double beta, const Vector & y, Vector & w){
+
+  assert(x.localLength >= n); //Test vector lengths
+  assert(y.localLength >= n);
+  const double * const xv = x.values;
+  const double * const yv = y.values;
+  double * const wv = w.values;
+
+  if(alpha == 1.0) {
+    return then([&](){ if(time != NULL) time = mytimer(); })
+    | stdexec::bulk(start_point, stdexec::par, n, [beta, xv, wv, yv, &](local_int_t i){ 
+        wv[i] = xv[i] + beta*yv[i]; })
+    | then([&](){ if(time != NULL) time = mytimer() - time; });
+  }
+  else if(beta == 1.0){
+    return then([&](){ if(time != NULL) time = mytimer(); })
+    | stdexec::bulk(start_point, stdexec::par, n, [alpha, xy, wv, yv, &](local_int_t i){ 
+        wv[i] = alpha*xv[i] + yv[i]; })
+    | then([&](){ if(time != NULL) time = mytimer() - time; });
+  }
+  else{
+    return then([&](){ if(time != NULL) time = mytimer(); })
+    | stdexec::bulk(start_point, stdexec::par, n, [alpha, beta, xy, wv, yv, &](local_int_t i){ 
+        wv[i] = alpha*xv[i] + beta*yv[i]; })
+    | then([&](){ if(time != NULL) time = mytimer() - time; });
+  }
+}
