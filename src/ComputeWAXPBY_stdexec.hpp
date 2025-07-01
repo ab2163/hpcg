99ebea4c0ca05f55aa67c128c10d50c1d3995f3d
Ajinkya Bhalerao <jinks.b99@gmail.com>
2025-07-01 19:47:51 +0100

    Revert "Changed SPMV to refer to SPMV_stdexec (which uses _ref) and code worked..."
    This reverts commit fb8037542a1990b0b912bcbc9df18eb5590dc53f.

--- src/ComputeWAXPBY_stdexec.hpp
+++ src/ComputeWAXPBY_stdexec.hpp
@@ -24,18 +24,17 @@
 auto ComputeWAXPBY_stdexec(double * time, const local_int_t n, const double alpha, const Vector & x,
     const double beta, const Vector & y, Vector & w){
 
-  /*
   assert(x.localLength >= n); //Test vector lengths
   assert(y.localLength >= n);
   const double * const xv = x.values;
   const double * const yv = y.values;
   double * const wv = w.values;
-  */
+
   //NEED TO FIND A WAY OF RETURNING ALTERNATIVE SENDERS
   //IF ALPHA = 1 OR BETA = 1!
-  return stdexec::then([&, time, n, alpha, beta](){ /*if(time != NULL) *time -= mytimer();*/
+  return stdexec::then([&, time, n, alpha, beta](){ if(time != NULL) *time -= mytimer();
      ComputeWAXPBY_ref(n, alpha, x, beta, y, w);
-     /*if(time != NULL) *time += mytimer();*/
+     if(time != NULL) *time += mytimer();
   });/*})
   | stdexec::bulk(stdexec::par, n, [&, alpha, beta, xv, wv, yv](local_int_t i){ 
       wv[i] = alpha*xv[i] + beta*yv[i]; })
