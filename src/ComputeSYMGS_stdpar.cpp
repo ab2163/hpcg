db9cdd1abae0da2a4df7047d2780de24e7fbd42b
Ajinkya Bhalerao <jinks.b99@gmail.com>
2025-07-03 20:03:12 +0100

    Realised stdpar implementation of SYMGS was taking too long with nvc++... Have made the stdpar version simply call the reference version Code runs properly now in stdpar

--- src/ComputeSYMGS_stdpar.cpp
+++ src/ComputeSYMGS_stdpar.cpp
@@ -2,66 +2,9 @@
 #include <numeric>
 #include <execution>
 
-#include "ComputeSYMGS_stdpar.hpp"
+#include "ComputeSYMGS_ref.hpp"
 
-int ComputeSYMGS_stdpar( const SparseMatrix & A, const Vector & r, Vector & x) {
-
-  assert(x.localLength==A.localNumberOfColumns); // Make sure x contain space for halo values
-
-#ifndef HPCG_NO_MPI
-  ExchangeHalo(A,x);
-#endif
-
-  const local_int_t nrow = A.localNumberOfRows;
-  double ** matrixDiagonal = A.matrixDiagonal;  // An array of pointers to the diagonal entries A.matrixValues
-  const double * const rv = r.values;
-  double * const xv = x.values;
-
-  for (local_int_t i=0; i< nrow; i++) {
-    const double * const currentValues = A.matrixValues[i];
-    const local_int_t * const currentColIndices = A.mtxIndL[i];
-    const int currentNumberOfNonzeros = A.nonzerosInRow[i];
-    const double  currentDiagonal = matrixDiagonal[i][0]; // Current diagonal value
-    double sum = rv[i]; // RHS value
-
-    sum -= std::transform_reduce(std::execution::par, 
-      currentValues, 
-      currentValues + currentNumberOfNonzeros,
-      currentColIndices,
-      0.0,
-      std::plus<>(),
-      [&](double matrixVal, int ind) { return matrixVal*xv[ind]; }
-    );
-
-    sum += xv[i]*currentDiagonal; // Remove diagonal contribution from previous loop
-
-    xv[i] = sum/currentDiagonal;
-
-  }
-
-  // Now the back sweep.
-
-  for (local_int_t i=nrow-1; i>=0; i--) {
-    const double * const currentValues = A.matrixValues[i];
-    const local_int_t * const currentColIndices = A.mtxIndL[i];
-    const int currentNumberOfNonzeros = A.nonzerosInRow[i];
-    const double  currentDiagonal = matrixDiagonal[i][0]; // Current diagonal value
-    double sum = rv[i]; // RHS value
-
-    sum -= std::transform_reduce(std::execution::par, 
-      currentValues, 
-      currentValues + currentNumberOfNonzeros,
-      currentColIndices,
-      0.0,
-      std::plus<>(),
-      [&](double matrixVal, int ind) { return matrixVal*xv[ind]; }
-    );
-
-    sum += xv[i]*currentDiagonal; // Remove diagonal contribution from previous loop
-
-    xv[i] = sum/currentDiagonal;
-  }
-
-  return 0;
+int ComputeSYMGS_stdpar(const SparseMatrix & A, const Vector & r, Vector & x) {
+  return ComputeSYMGS_ref(A, r, x);
 }
 
