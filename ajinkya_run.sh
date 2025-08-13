64110f35c0e5b903e55503cfb51b49ce33aab2d9
Ajinkya Bhalerao <jinks.b99@gmail.com>
2025-08-13 10:08:03 +0100

    Using starts_on for senders in CG_stdexec (currently only works for CPU execution)

--- ajinkya_run.sh
+++ ajinkya_run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cd build/bin
-export OMP_NUM_THREADS=4
+export OMP_NUM_THREADS=2
 export OMP_PROC_BIND=spread
-export OMP_PLACES=threads
+export OMP_PLACES=cores
 ./xhpcg
 cat HPCG-Benchmark_3.1*.txt
 cd ../..
